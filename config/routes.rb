7a7600eeb3d70424fafc49c9fce8e0a0b40a3699
Soren Umstot <soren.umstot@gmail.com>
2022-12-26 13:13:53 +0900

    Added messages controller with a create method

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   resources :chatrooms, only: :show
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :chatrooms, only: :show do
+    resources :messages, only: :create
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
