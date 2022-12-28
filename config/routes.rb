9feae419a0b2158fac616dcb19c0d23a7f77b822
Soren Umstot <soren.umstot@gmail.com>
2022-12-28 13:40:23 +0900

    added :new to resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :chatrooms, only: :show
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :chatrooms, only: [:index, :create, :destroy]
+  resources :chatrooms, only: [:index, :create, :new, :destroy]
   resources :chatrooms, only: :show do
     resources :messages, only: :create
   end
