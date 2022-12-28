5b9d6f12ff70f26679f8f6ac18b184bb73fb505e
Soren Umstot <soren.umstot@gmail.com>
2022-12-28 11:24:50 +0900

    Added a sidebar that contains link to different channels, added an add channel button, and added an index page as well that needs to be built

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,20 @@
 class ChatroomsController < ApplicationController
+  def new
+    @chatroom = Chatroom.new
+  end
+  
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+  end
+
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+  end
+
+  private
+
+  def chatroom_params
+    params.require(:chatroom).permit(:name)
   end
 end
