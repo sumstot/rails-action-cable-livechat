d2cf4ecf99c1485b1c78d62bfc4511d78edfb6f0
Soren Umstot <soren.umstot@gmail.com>
2022-12-28 13:33:20 +0900

    Need to figue out routes for adding a create chatroom path

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,9 +2,14 @@ class ChatroomsController < ApplicationController
   def new
     @chatroom = Chatroom.new
   end
-  
+
   def create
     @chatroom = Chatroom.new(chatroom_params)
+    if @chatroom.save
+      redirect_to chatroom_path
+    else
+      render 'chatrooms/show', status: :unprocessable_entity
+    end
   end
 
   def show
