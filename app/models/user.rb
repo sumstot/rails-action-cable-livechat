238ba41997d07c9b2bd50230119767cd8d610aab
Soren Umstot <soren.umstot@gmail.com>
2023-01-04 18:24:42 +0900

    Updated more styling and changed colors of links on main page. Also changed the default avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_one_attached :avatar
   # scope :all_except, -> (user) { where.not{id: user}}
 
 end
